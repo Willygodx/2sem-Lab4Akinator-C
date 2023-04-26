d3bfceedbf93cc26f616f0d26ad1db2379f3846b
willygodx <enotland34@yandex.ru>
2023-04-26 12:36:28 +0300

    1

--- Lab4Akinator/log.c
+++ Lab4Akinator/log.c
@@ -3,9 +3,9 @@
 void log(FILE* treeFile, char* message, const int* value) {
     char buffer[256];
     fseek(treeFile, 0, SEEK_END);
-    time_t timeInNumbers = time(NULL);
+    time_t timeAtTheMoment = time(NULL);
     struct tm timeFormat;
-    localtime_r(&timeInNumbers, &timeFormat);
+    localtime_r(&timeAtTheMoment, &timeFormat);
     strftime(buffer, 256, "%Y-%m-%d %H:%M:%S", &timeFormat);
     if (value == NULL) fprintf(treeFile, "[%s]: username INPUT: %s\n", buffer, message);
     else if (message == NULL) fprintf(treeFile, "[%s]: username INPUT: %d\n", buffer, *value);
